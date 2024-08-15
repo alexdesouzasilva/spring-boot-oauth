bf181626a7381362922de40a04f00374e70eee35
Alex <alexdesouzasilva94@gmail.com>
2024-08-15 05:51:55 -0300

    add componente SecurityFilterChain and PasswordEncoder - AND: criptografia da senha do usuário

---
+++ src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.devsuperior.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable()); // desativando segurança de ataque csrf. API Rest não guarda dados em sessão
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); // Permitindo qualquer requisição.
+        return http.build();
+    }
+
+    
+}
